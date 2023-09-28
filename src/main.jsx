3bd602f50f6eb0c42f53bfc7cdb299276f77f2a3
haldun atar <atarhaldun@gmail.com>
2023-09-28 09:01:11 +0200

    remove pokemon

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-import pokemon from "pokemontcgsdk";
-pokemon.configure({ apiKey: "b41d08ac-a4cf-4b11-90e1-f8bafc11c82d" });
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
